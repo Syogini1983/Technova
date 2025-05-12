73f1387b1b7d50e23dcb56bfd92ba572b4733dd7
Syogini1983 <143272542+Syogini1983@users.noreply.github.com>
2025-05-12 17:34:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Welcome to TechNova's Inventory App!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
+print("Hello from Jenkins and Docker!")
